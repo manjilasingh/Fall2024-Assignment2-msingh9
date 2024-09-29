f3c7e1cab9490244e245fe77cf35d458d1e362e5
Manjila Singh <singh.manjila02@gmail.com>
2024-09-29 11:15:19 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment2_msingh9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
